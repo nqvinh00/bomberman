08663c7a547efc6601632416264fe1ab190c02ae
nqvinh00 <nqvinh00@gmail.com>
2020-11-06 12:10:17 +0700

    create Bomb object

---
+++ src/bomberman/Bomb/Bomb.java
@@ -0,0 +1,22 @@
+package bomberman.Bomb;
+
+import bomberman.Entities.Entity;
+import bomberman.Entities.MovableEntity;
+import bomberman.Graphics.Screen;
+
+public class Bomb extends MovableEntity {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Screen screen) {
+
+    }
+
+    @Override
+    public boolean isCollided(Entity e) {
+        return false;
+    }
+}
