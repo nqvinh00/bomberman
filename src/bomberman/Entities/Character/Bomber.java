376dcb12ded5fa9374e60cd923c26a2dde3c5f3c
nqvinh00 <nqvinh00@gmail.com>
2020-11-06 12:10:00 +0700

    create Character object

---
+++ src/bomberman/Entities/Character/Bomber.java
@@ -0,0 +1,21 @@
+package bomberman.Entities.Character;
+
+import bomberman.Entities.Entity;
+import bomberman.Graphics.Screen;
+
+public class Bomber extends Character {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Screen screen) {
+
+    }
+
+    @Override
+    public boolean isCollided(Entity e) {
+        return false;
+    }
+}
---
+++ src/bomberman/Entities/Character/Character.java
@@ -0,0 +1,17 @@
+package bomberman.Entities.Character;
+
+import bomberman.Entities.MovableEntity;
+import bomberman.Graphics.Screen;
+
+public abstract class Character extends MovableEntity {
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Screen screen) {
+
+    }
+}
